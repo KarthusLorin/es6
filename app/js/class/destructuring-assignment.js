75e4bca65d06679d318db619690a1d15e89281cc
KarthusLorin <karthuslorin@gmail.com>
2018-08-02 23:57:19 +0800

    add array destructuring-assignment

--- app/js/class/destructuring-assignment.js
+++ app/js/class/destructuring-assignment.js
@@ -1,11 +1,53 @@
 {
+  // 数组解构方式——基本
+  let a, b, c, rest;
+  [a, b, c] = [1, 2];
+  console.log(a, b, c)   // 1 2 undefined
+}
+
+{
+  // 数组解构方式——设置默认值
+  let a, b, c, rest;
+  [a, b, c = 3] = [1, 2];
+  console.log(a, b, c)   // 1 2 3
+}
+
+{
+  // 数组解构方式——跳跃赋值
+  let a, b, c, rest;
+  [a, , , b] = [1, 2, 3, 4, 5]
+  console.log(a, b)   // 1 4
+}
+
+{
+  // 数组解构方式——最后一个变量设为数组
   let a, b, rest;
-  [a, b] = [1, 2];
+  [a, b, ...rest] = [1, 2, 3, 4, 5, 6]
+  console.log(a, b, rest)
+}
+
+{
+  // 数组解构运用1——交换赋值
+  let a = 1;
+  let b = 2;
+  [a, b] = [b, a];
+  console.log(a, b)     // 2 1
+}
+
+{
+  // 数组解构运用2——计算结果赋值
+  function f () {
+    return [1, 2]
+  }
+
+  let a, b;
+  [a, b] = f();
   console.log(a, b)
 }
 
 {
-  let a, b, rest;
-  [a, b, ...rest] = [1, 2, 3, 4, 5, 6]
-  console.log(a, b, rest)
+  // 对象解构
+  let a, b;
+  ({a, b} = {a: 1, b: 2})
+  console.log(a, b)
 }
