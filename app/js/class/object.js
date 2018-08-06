f08e36d8d26f278316ba99f2f0f42779abd60cb9
KarthusLorin <karthuslorin@gmail.com>
2018-08-06 23:26:46 +0800

    add object

---
+++ app/js/class/object.js
@@ -0,0 +1,56 @@
+{
+  // 简洁表示法
+  let o = 1;
+  let k = 2;
+  let es5 = {
+    o: o,
+    k: k
+  }
+  let es6 = {
+    o,
+    k
+  }
+  console.log(es5, es6)
+
+  let es5_method = {
+    hello: function () {
+      console.log('hello')
+    }
+  }
+  let es6_method = {
+    hello () {
+      console.log('hello')
+    }
+  }
+  console.log(es5_method.hello(), es6_method.hello())
+}
+
+{
+  // 属性表达式
+  let a = 'b'
+  let es5_obj = {
+    a: 'c',
+    b: 'c'
+  }
+  let es6_obj = {
+    [a]: 'c'
+  }
+  console.log(es5_obj, es6_obj)
+}
+
+{
+  // 新增api
+  // 判断两个值是否相等
+  console.log('字符串', Object.is('abc', 'abc'))
+  // 合并对象
+  console.log('拷贝', Object.assign({a: 'a'}, {b: 'b'}))
+  //
+  let test = {
+    k: 123,
+    o: 456
+  }
+  // entries遍历
+  for (let [key, value] of Object.entries(test)) {
+    console.log([key, value])
+  }
+}
