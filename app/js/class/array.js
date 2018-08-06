0b723479ace7c636eb3c7e55fe94b160bbe615b8
KarthusLorin <karthuslorin@gmail.com>
2018-08-06 22:21:01 +0800

    add array

--- app/js/class/array.js
+++ app/js/class/array.js
@@ -30,3 +30,31 @@
   console.log('fill-7', [1, 'a', undefined].fill(7, 1, 3))  // [1,7,7]
 }
 
+{
+  for (let index of ['1', 'c', 'ks'].keys()) {
+    console.log('keys', index)    // 0 1 2
+  }
+  for (let value of ['1', 'c', 'ks'].values()) {
+    console.log('keys', value)    // 1 c ks
+  }
+  for (let [index, value] of ['1', 'c', 'ks'].entries()) {
+    console.log('values', value)  // 1 c ks
+  }
+}
+
+{
+  // 搜索并返回第一个符合条件的数组值
+  console.log([1, 2, 3, 4, 5, 6].find((item) => {
+    return item > 3     // 4
+  }))
+  // 搜索并返回第一个符合条件的数组下标
+  console.log([1, 2, 3, 4, 5, 6].findIndex((item) => {
+    return item > 3     // 3
+  }))
+}
+
+{
+  // 数组是否包含
+  console.log('number', [1,2,NaN].includes(1))    // true
+  console.log('number', [1,2,NaN].includes(NaN))  // true
+}
