4cfb7faa242af034cfd2b39a9d3b80f2a357a413
KarthusLorin <karthuslorin@gmail.com>
2018-08-13 23:21:00 +0800

    add module

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,10 @@
-import 'babel-polyfill'
-import './class/decorator'
+// import 'babel-polyfill'
+// import './class/module'
+
+// import {A, test, Hello} from "./class/module"
+
+// import * as moduleTest from './class/module'
+
+import moduleTest from './class/module'
+
+console.log(moduleTest.A)
